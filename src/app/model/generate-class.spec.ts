f5ae2238a7a43b427d6bf26e923aae7e5287baa1
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-02-07 11:44:46 +0700

    use generate class

---
+++ src/app/model/generate-class.spec.ts
@@ -0,0 +1,7 @@
+import { GenerateClass } from './generate-class';
+
+describe('GenerateClass', () => {
+  it('should create an instance', () => {
+    expect(new GenerateClass()).toBeTruthy();
+  });
+});
