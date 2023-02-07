5334450411fcefd748eccf23c241e31230c52e15
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-02-07 16:10:15 +0700

    life cycle

--- src/app/generate-component/generate-component.component.ts
+++ src/app/generate-component/generate-component.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-generate-component',
   templateUrl: './generate-component.component.html',
   styleUrls: ['./generate-component.component.scss']
 })
-export class GenerateComponentComponent {
+export class GenerateComponentComponent implements OnInit {
+
+  public name:String = "";
+  public isDisable:boolean;
+
+  abc : Record<string, boolean> = {};
+
+  constructor() {
+    this.isDisable = true;
+  }
+
+  ngOnInit(): void {
+    this.name = "abc";
+    console.log("abc");
+  }
 
 }
