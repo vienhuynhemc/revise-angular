c10c022d563da55902cca6c21e0a1b9fe2080c29
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-02-07 12:05:21 +0700

    use generate component in module

--- src/app/generate-module/generate-module.module.ts
+++ src/app/generate-module/generate-module.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentInModuleComponent } from './component-in-module/component-in-module.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ComponentInModuleComponent
+  ],
   imports: [
     CommonModule
   ]
