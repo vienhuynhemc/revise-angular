1349f27209f741e833c07f0e17cb7f00d6362e7f
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-02-08 15:57:42 +0700

    practice output input

--- src/app/modules/my-store/components/content/content.component.ts
+++ src/app/modules/my-store/components/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent {
+
+  @Input() products!: {
+    id: number,
+    name: string,
+    description: string,
+  }[];
+
+  @Output() productSelected = new EventEmitter<{ id: number, name: string, description: string }>();
+
+  public share(product: { id: number, name: string, description: string }): void {
+    this.productSelected.emit(product);
+  }
 
 }
--- src/app/modules/my-store/pages/home-page/home-page.component.ts
+++ src/app/modules/my-store/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
+
+  public products: { id: number, name: string, description: string }[];
+
+  constructor() {
+    this.products = [
+      {id: 1, name: 'abc', description: 'def'},
+      {id: 2, name: 'abc', description: 'def'},
+      {id: 3, name: 'abc', description: ''},
+    ]
+  }
+
+  public getProduct(product: { id: number, name: string, description: string }): void {
+    this.products = this.products.filter(value => value.id != product.id);
+  }
 
 }
